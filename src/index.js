7c4dfffe46ee5868a5bac4b1038de097c98c684f
SolidRules <amikheev1993@gmail.comm>
2022-07-16 20:13:06 +0300

    init: start towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix == undefined || matrix == 0) {return []} else { 
+        for (let i = 1; i < matrix.length; i += 2) {
+          matrix[i].sort((a, b) => b - a);
+        }
+        return matrix.flat()
+      }
 }
